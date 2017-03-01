8068f14637110c38bfbefcf459c815eb12ebd75b
Andy <zhou_jian007@126.com>
2017-03-01 23:10:06 +0800

    增加beduilayer返回按钮操作

--- Classes/BedUILayer.cpp
+++ Classes/BedUILayer.cpp
@@ -16,6 +16,15 @@ bool BedUILayer::init()
 {
 	Node* csbnode = CSLoader::createNode("bedUiLayer.csb");
 	this->addChild(csbnode);
-
+	cocos2d::ui::Widget* backbtn = (cocos2d::ui::Widget*)csbnode->getChildByName("backbtn");
+	backbtn->addTouchEventListener(CC_CALLBACK_2(BedUILayer::onBack, this));
 	return true;
+}
+
+void BedUILayer::onBack(cocos2d::Ref *pSender, cocos2d::ui::Widget::TouchEventType type)
+{
+	if (type == ui::Widget::TouchEventType::ENDED)
+	{
+		this->removeFromParentAndCleanup(true);
+	}
 }
--- Classes/BedUILayer.h
+++ Classes/BedUILayer.h
@@ -14,6 +14,7 @@ public:
 
 	CREATE_FUNC(BedUILayer);
 private:
+	void onBack(cocos2d::Ref *pSender, cocos2d::ui::Widget::TouchEventType type);
 };
 #endif
 
